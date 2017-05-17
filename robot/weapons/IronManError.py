39061c98ce17cc59445377c37565e55ece0e982e
xiezhida <xiezhida@renrenche.com>
2017-05-17 11:54:45 +0800

    为以后预留的 爬虫自有的错误类

---
+++ robot/weapons/IronManError.py
@@ -0,0 +1,38 @@
+# coding=utt-8
+
+
+# exception classes
+class Error(Exception):
+    """Base class for ConfigParser exceptions."""
+
+    def _get_message(self):
+        """Getter for 'message'; needed only to override deprecation in
+        BaseException."""
+        return self.__message
+
+    def _set_message(self, value):
+        """Setter for 'message'; needed only to override deprecation in
+        BaseException."""
+        self.__message = value
+
+    # BaseException.message has been deprecated since Python 2.6.  To prevent
+    # DeprecationWarning from popping up over this pre-existing attribute, use
+    # a new property that takes lookup precedence.
+    message = property(_get_message, _set_message)
+
+    def __init__(self, msg=''):
+        self.message = msg
+        Exception.__init__(self, msg)
+
+    def __repr__(self):
+        return self.message
+
+    __str__ = __repr__
+
+
+class IronManError(Error):
+    def __init__(self, message):
+        Error.__init__(self, message)
+        self.message = message
+        self.args = (message,)
+
